f9949fc65db2af08177c12a469431d65b420d91e
Sugiarto Cokrowibowo <sugiarto.cokrowibowo.id@gmail.com>
2021-02-22 09:17:38 +0800

    new commit

---
+++ src/main/java/fosalgo/Main.java
@@ -0,0 +1,14 @@
+package fosalgo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nama = sc.nextLine();
+        
+        System.out.println("Halo "+nama);
+    }
+    
+}
